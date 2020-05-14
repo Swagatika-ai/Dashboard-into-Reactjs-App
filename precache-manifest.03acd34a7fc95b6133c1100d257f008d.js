ad51a39cad6dbae857d3b12d991d8f5941028758
Swagatika-ai <swagatika.som123@gmail.com>
2020-05-13 18:31:44 -0700

    Updates

---
+++ precache-manifest.03acd34a7fc95b6133c1100d257f008d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15e1e88a3fee09866b43de81057afb2",
+    "url": "/Embedding-Dashboard-into-Reactjs-App/index.html"
+  },
+  {
+    "revision": "ce6da4e303e2bbb06ba1",
+    "url": "/Embedding-Dashboard-into-Reactjs-App/static/js/2.3962f933.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Embedding-Dashboard-into-Reactjs-App/static/js/2.3962f933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c378781a5ad121bd58d7",
+    "url": "/Embedding-Dashboard-into-Reactjs-App/static/js/main.eb98a8f5.chunk.js"
+  },
+  {
+    "revision": "958c7ddf21d688c5ea17",
+    "url": "/Embedding-Dashboard-into-Reactjs-App/static/js/runtime-main.b58da8cb.js"
+  }
+]);
